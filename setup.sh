3b7cd70dd64c36f41103d5e355c6edcbb8096b60
Linxius <linique@163.com>
2019-03-22 16:08:13 +0800

    change camera_init to velodyne in pkg loam

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-export HUSKY_DESCRIPTION=~/ros/myHusky/src/husky_description/urdf/description.xacro
 source ./devel/setup.bash
+export HUSKY_DESCRIPTION=~/ros/sensorplus/src/husky_description/urdf/description.xacro
+export HUSKY_DESCRIPTION=~/ros/myHusky/src/husky_description/urdf/description.xacro
